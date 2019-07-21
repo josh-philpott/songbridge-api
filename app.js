475cdbcc5c53cf35977fb9b133636ad4a711b9cd
Joshua Philpott <jphil529@gmail.com>
2019-07-21 14:29:51 -0600

    cors configuration

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(cors())
+app.use(
+  cors({
+    origin: '*',
+    credentials: true
+  })
+)
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', indexRouter)
