07ead4ef0e1eb10bf08d385766e8a0d0f3e77db6
Joshua Philpott <jphil529@gmail.com>
2019-07-21 11:46:10 -0600

    debug

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express()
 
 app.listen(3002)
 
-app.use(cors())
+app.use(cors({ credentials: true }))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
