b997673ca41ded38dfc2b4474aa3fe488c3fb3c0
Joshua Philpott <jphil529@gmail.com>
2019-07-21 14:32:58 -0600

    debug

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(
   cors({
-    origin: '*',
+    origin: 'songbridge.netlify.com',
     credentials: true
   })
 )
